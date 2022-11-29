1b61fc0364fb8f513f384482bfb9e4a379efdbfe
josephjensen <joe@cryptonumerics.com>
2022-11-28 19:44:10 -0800

    route and controller for updating thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,36 +3,49 @@ const { User, Thought } = require('../models');
 module.exports = {
     getThoughts(req, res) {
         Thought.find()
-        .then(async (thoughts) => {return res.json(thoughts)})
+            .then(async (thoughts) => { return res.json(thoughts) })
     },
     getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
-          .then(async (thought) =>
-            !thought
-              ? res.status(404).json({ message: 'No thought with that ID' })
-              : res.json({
-                  thought,
-                })
-          )
-          .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-          });
-      },
+            .then(async (thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json({
+                        thought,
+                    })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
     createThought(req, res) {
         Thought.create(req.body)
-        .then((thought) => {
-            return User.findOneAndUpdate(
-                { username: req.body.username },
-                { $push: { thoughts: thought._id}},
-                { new: true }
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { username: req.body.username },
+                    { $push: { thoughts: thought._id } },
+                    { new: true }
+                )
+            })
+            .then((updatedUser) =>
+                !updatedUser
+                    ? res.status(404).json({ message: 'Thought created, but no user found with that username.' })
+                    : res.json(`Created new thought from ${updatedUser.username}!`)
             )
-        })
-        .then((updatedUser) => 
-            !updatedUser
-            ? res.status(404).json({ message: 'Thought created, but no user found with that username.' })
-            : res.json(`Created new thought from ${updatedUser.username}!`)    
-    )
-    .catch((err) => res.status(500).json(err))
+            .catch((err) => res.status(500).json(err))
+    },
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought with this id" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err))
     }
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { getThoughts, createThought, getSingleThought } = require('../../controllers/thoughtController');
+const { getThoughts, createThought, getSingleThought, updateThought } = require('../../controllers/thoughtController');
 
 router.route('/').get(getThoughts).post(createThought)
 
-router.route('/:thoughtId').get(getSingleThought)
+router.route('/:thoughtId').get(getSingleThought).put(updateThought)
 
 module.exports = router;
