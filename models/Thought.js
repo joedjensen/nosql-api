11df8976b62f508e391ce292fe86cf7b2754a76f
josephjensen <joe@cryptonumerics.com>
2022-11-28 17:16:40 -0800

    initial implementation of models

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./User');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+function formatDate(date) {
+    return date;
+}
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
